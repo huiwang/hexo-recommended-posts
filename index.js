c9ab75095a33e9b00dca224a2243f5cbbe8cadb2
Hui Wang <hedonplay@gmail.com>
2017-12-22 16:58:38 +0100

    upload posts to server

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+hexo.extend.generator.register('recommended_posts_uploader', require('./lib/uploader'));
 hexo.extend.tag.register('recommended_posts', require('./lib/recommender')(hexo), {async: true});
