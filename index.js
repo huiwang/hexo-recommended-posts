4aad0f2d54680578ff71a641843c6f4e0df983a2
Hui Wang <hedonplay@gmail.com>
2017-12-25 18:07:48 +0100

    add tag

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 hexo.extend.helper.register('recommended_posts', require('./lib/recommend_helper'))
 
+hexo.extend.tag.register('recommended_posts', require('./lib/recommend_tag')(hexo))
+
 hexo.extend.console.register('recommend', 'Downlaod recommended posts from recommendation server', {
     usage: '',
     arguments: []
