f21b9abcb94292b5996e61246a1b27c31a0e5683
Hui Wang <hedonplay@gmail.com>
2018-01-03 08:39:31 +0100

    zero config support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-hexo.extend.helper.register('recommended_posts', require('./lib/recommend_helper'))
+hexo.extend.helper.register('recommended_posts', require('./lib/recommend_helper'));
+hexo.extend.filter.register('after_post_render', require('./lib/recommend_filter'));
 hexo.extend.console.register('recommend', 'Downlaod recommended posts from recommendation server', {
     usage: '',
     arguments: []
